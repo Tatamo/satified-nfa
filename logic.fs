a3d73ae33f5a0391f771dabad2d0068f5648072a
Tatamo <nsorder@gmail.com>
2019-07-22 14:52:45 +0900

    convert SemiLiteral to Literal

--- logic.fs
+++ logic.fs
@@ -12,9 +12,25 @@ type Literal =
   | Atomic of Atomic
   | LNot of Not<Atomic>
 
+type MultiNotAtomic =
+  | DoubleNot of Not<Not<Atomic>>
+  | MultiNot of Not<MultiNotAtomic>
+
 type SemiLiteral =
   | Literal of Literal
-  | SLNot of Not<SemiLiteral>
+  | MultiNotSemiLiteral of MultiNotAtomic
+
+let rec literalize semiLiteral =
+  match semiLiteral with
+  | Literal(a) -> a
+  | MultiNotSemiLiteral(mn) ->
+    match mn with
+    | DoubleNot(Not (Not x)) -> Atomic x
+    | MultiNot(Not x) ->
+      match x with
+      | DoubleNot(Not (Not x)) -> LNot (Not x)
+      | MultiNot(Not x) -> literalize(MultiNotSemiLiteral x)
+
 
 type Term =
   | SemiLiteral of SemiLiteral
