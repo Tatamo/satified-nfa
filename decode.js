7e1c32e925390efef937be4dbdd0bf003957697c
Tatamo <nsorder@gmail.com>
2019-07-24 03:25:50 +0900

    fix broken decoder

--- decode.js
+++ decode.js
@@ -1,7 +1,7 @@
 function decode(input) {
   return input.trim().split(" ").map(e => {
     const flag = e[0] !== "~";
-    const name = +(flag ? e : e.substring(1));
-    return { flag, num: name };
+    const name = flag ? e : e.substring(1);
+    return { flag, name };
   }).sort((a, b) => a.name - b.name).map(({ flag, name }) => `${name}: ${flag ? 1 : 0}`).join("\n");
 }
