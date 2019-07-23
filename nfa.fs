c964de0fba696c0f50122d4be51cc606e1fbfcbe
Tatamo <nsorder@gmail.com>
2019-07-23 11:34:43 +0900

    input to props

--- nfa.fs
+++ nfa.fs
@@ -31,11 +31,21 @@ let nfa = (alphabets, states, rules, start, accepts)
 let p (state:State) (index:int) = Var("p" + (match state with State(i) -> i.ToString()) + "-" + index.ToString() )
 let x (input:char) (index:int) = Var("x" + string input + "-" + index.ToString())
 
+let input2Props (input:string) =
+  input
+  |> Seq.toList
+  |> Seq.indexed
+  |> Seq.map (fun (index, c) ->
+    let char2bool c = (c <> '0') in
+    if char2bool c then
+      OrForm.Literal(Atomic (x c index)) else
+      OrForm.Literal(LNot (Not (x c index)))
+  )
 // for all states that is not start state, px0=false
-let pInitials =
+let pInitials start =
   states
   |> Seq.map (fun state ->
-    if states.Contains state then
+    if state = start then
       Atomic (p state 0) else
       LNot (Not (p state 0)))
   |> OrForm.Or
@@ -55,7 +65,7 @@ let rules2Props' n =
 let rules2Props n = seq {0 .. n-1} |> Seq.collect rules2Props'
 
 let terms =
-  [seq [pInitials]; rules2Props n]
+  [input2Props inputString;seq [pInitials start]; rules2Props n]
   |> Seq.concat
   |> CNF.And
 
