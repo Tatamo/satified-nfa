276469086d9194469a698030fcaa5a1da7c525c8
Tatamo <nsorder@gmail.com>
2019-07-25 07:16:57 +0900

    decode and format SAT output

--- decode.js
+++ decode.js
@@ -1,7 +1,40 @@
 function decode(input) {
-  return input.trim().split(" ").map(e => {
-    const flag = e[0] !== "~";
-    const name = flag ? e : e.substring(1);
-    return { flag, name };
-  }).sort((a, b) => a.name - b.name).map(({ flag, name }) => `${name}: ${flag ? 1 : 0}`).join("\n");
+  const vars = input.trim().split(" ").map(s => {
+    const flag = s[0] !== "~";
+    if (!flag) s = s.substring(1);
+    const name = s[0];
+    s = s.substring(1);
+    if (name === "x") return { flag, name, index: +s };
+    else if (name === "p") return { flag, name, pos: s.split("_") };
+  })
+  const result = { x: [], p: [] };
+  const xlist = vars.filter(({ name }) => name === "x");
+  for (const { flag, index } of xlist) {
+    result.x[index - 1] = flag ? 1 : 0;
+  }
+  const plist = vars.filter(({ name }) => name === "p")
+  //    .sort(({ pos: [i1, s1] }, { pos: [i2, s2] }) => i1 - i2 === 0 ? i1 - i2 : s1 - s2);
+  for (const { flag, name, pos: [index, state] } of plist) {
+    if (result.p[index] === undefined) result.p[index] = [];
+    result.p[index][state] = flag ? 1 : 0;
+  }
+  return result;
+}
+
+function formatVars(vars) {
+  console.log(vars.x.join(" "));
+  for (const l of vars.p) {
+    console.log(l.join(" "));
+  }
+}
+
+const input = require("fs").readFileSync("/dev/stdin", "utf8");
+if (input.trim() === "~") {
+  console.log("UNSAT");
+} else {
+  const vars = decode(input)
+  formatVars(vars);
+  console.log("SAT");
+  console.log(`input string: ${vars.x.join("")}`);
+
 }
